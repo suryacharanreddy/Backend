6039dc5e418d775f482bde684336fe4e7d78e290
suryacharanreddy <suryacharan4494@gmail.com>
2025-10-17 09:12:34 +0530

    update_17-10

--- JAVA/Small.java
+++ JAVA/Small.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
-public class Small {
-    public static void main(String []args){
-        Scanner pole=new Scanner(System.in);
-        System.out.print("Enter a String :");
-        String str=pole.nextLine();
-        System.out.println("You entered : "+str);
-        pole.close();
+public class Small{
+    public static void main(String args[]){
+        int a=10;
+        for(int i=0;i<a;i++){
+            if(i%2==0){
+                System.out.println("First 5 Even Numbers :"+i);
+            }
+        }
     }
-    
 }
