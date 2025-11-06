8e32f58457eb1c51568bfd05b78e0df1cebe7edb
suryacharanreddy <suryacharan4494@gmail.com>
2025-11-06 12:22:51 +0530

    Java 6-11

---
+++ JAVA/Arrays-Strings.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Array{
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6,7,};
+        String[] str={"surya","charan","reddy"};
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(str));
+    }
+}
