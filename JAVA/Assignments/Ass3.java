175cf28d6d95bfee6eb0b7ac953eba2c881d5ad8
suryacharanreddy <suryacharan4494@gmail.com>
2025-11-19 13:49:38 +0530

    Assignments

---
+++ JAVA/Assignments/Ass3.java
@@ -0,0 +1,6 @@
+package pack1;
+class MathUtil{
+    public static int square(int n){
+        return n*n;
+    }
+}
---
+++ JAVA/Assignments/Ass4.java
@@ -0,0 +1,7 @@
+package pack1;
+public class Ass4{
+    public static void main(String []args){
+        int result=MathUtil.square(5);
+        System.out.println(result);
+    }
+}
