606148cbf02e5aaa6dd4d3bc7a89e1f08db1bf61
suryacharanreddy <suryacharan4494@gmail.com>
2025-12-17 12:22:32 +0530

    Exception Handling

---
+++ JAVA/ExceptionHandling/New.java
@@ -0,0 +1,9 @@
+class New{
+    public static void main(String []args){
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        String c=args[2];
+        System.out.println(a+b+c);
+        System.out.println("gm");
+    }
+}
